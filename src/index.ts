561d011292c2a95b14f35d76620bd089b660bcd0
Aksh-Konda <aksh.konda@gmail.com>
2020-09-05 17:35:27 +0530

    HTTP Server Basic

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,17 @@
-import { Rectangle as rect } from "./rectangle";
+import http from 'http';
 
-function solveRect(l, b) {
-    console.log("Solving for rectangle with l = "
-        + l + " and b = " + b);
-    rect(l, b, (err, rectangle) => {
-        if (err) {
-            console.log("ERROR: ", err.message);
-        }
-        else {
-            console.log("The area of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.area());
-            console.log("The perimeter of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.perimeter());
-        }
-    });
-    console.log("This statement after the call to rect()");
-};
 
-solveRect(-2, 2);
-solveRect(2, 4);
-solveRect(2, -5);
-solveRect(3, 2);
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log(req.headers);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>Hello, World!</h1></body></html>');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
