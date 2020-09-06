773f75f72c8d96f0708c7ee38703da57c07c2740
Aksh-Konda <aksh.konda@gmail.com>
2020-09-06 15:25:53 +0530

    Express Serve Static Files

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import express from 'express';
 import http from 'http';
+import morgan from 'morgan';
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
 
+app.get('/favicon.ico', function(req, res) { 
+    res.status(204);
+    res.end(); 
+});
+app.use(express.static(__dirname+'/../public'));
 app.use((req, res, next) => {
-    console.log(req.headers);
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
